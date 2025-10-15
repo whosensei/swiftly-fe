619183637886f30be54bafe8fa14c58d66b763c1
whosensei <sukunagojo965@gmail.com>
2025-10-16 01:21:08 +0530

    reverted-prev-changes

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,11 @@ export default function Home() {
       <div className="absolute top-0 left-0 right-0 z-10">
         <Navbar />
       </div>
-      <div className="flex justify-center flex-1 w-full overflow-x-hidden pt-[calc(var(--navbar-height,4rem))]">
+      <div className="flex justify-center flex-1 w-full overflow-x-hidden">
         <div className="flex flex-col flex-1 w-full max-w-3xl border-dashed overflow-x-hidden">
-          <div className="flex flex-col items-center justify-center min-h-[calc(100vh-var(--navbar-height,4rem))]">
-            <div className="sticky top-[calc(var(--navbar-height,4rem))] bg-background z-10 w-full flex flex-col items-center justify-center gap-8 pb-8">
-              <Hero />
-              <Shorten />
-            </div>
+          <div className="flex flex-col items-center justify-center flex-1 gap-8">
+            <Hero />
+            <Shorten />
           </div>
         </div>
       </div>
