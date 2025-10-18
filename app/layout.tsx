642d64fb43eabe49149c38991c05bf0c84f5490f
whosensei <sukunagojo965@gmail.com>
2025-10-19 00:47:10 +0530

    Added metadata and vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Instrument_Serif } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,8 +20,37 @@ const instrumentSerif = Instrument_Serif({
 });
 
 export const metadata: Metadata = {
-  title: "Swiftly",
-  description: "Shorten your URLs with Swiftly",
+  title: `Swiftly`,
+  description: `Supercharge your links`,
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@dipesshhhh',
+    creator: '@dipesshhhh',
+    title: `Swiftly`,
+    description: `Supercharge your links`,  
+    images: '/gradii-1200x630.png',
+  },
+
+  openGraph: {
+    title: `Swiftly`,
+    description: `Supercharge your links`,
+    siteName: `Swiftly`,
+    images: [
+      {
+        url: '/gradii-1200x630.png',
+        width: 1200,
+        height: 630,
+        alt: `Swiftly - URL Shortener`,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
@@ -30,9 +60,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} antialiased h-full`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
